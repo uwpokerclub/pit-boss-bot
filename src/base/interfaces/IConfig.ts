ddf0f1882ebe1d5fdc67a1e8c55f9427f9c9166c
Alexanderfzz <Alexanderfzz.haha@gmail.com>
2025-01-20 15:27:29 -0500

    feat: allow bot to add role to logged in clients
    The bot will assign the role "Account linked" role to users upon successful login.
    The role will give users access to ranking commands (not yet implemented), and prevent users from calling /login command again. This prevents duplicate entries in the DB, on top of the existing measures to discourage this.
    The bot MUST have the permission "Manage Roles" in the guild, and the role of the bot must be place higher than the role(s) the bot will be assigning to users in the hierarchy.

--- src/base/interfaces/IConfig.ts
+++ src/base/interfaces/IConfig.ts
@@ -2,6 +2,7 @@ export default interface IConfig {
     token: string;
     clientId: string;
     guildId: string;
+    verifiedRoleId: string;
     brevo: {
         brevoKey: string, 
         authEmailAddress: string;
